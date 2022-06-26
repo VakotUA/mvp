b4cf0672d778b3682798fa17bd4f71855a121cb4
VakotUA <vakotua.g@gmail.com>
2022-06-26 17:08:31 +0300

    initial commit + header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './components/header/Header'
+import LoginForm from './components/login/loginForm'
+import styles from './App.module.css'
+import TestUserLogo from './assets/userLogo.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const adminUser = {
+  email: 'vakotua.g@gmail.com',
+  password: '1488',
+  img: TestUserLogo,
+  // TODO: Стату пользователя (посетитель, работник и тд.)
 }
 
-export default App;
+function App() {
+  const [user, setUser] = useState({ name: '', email: '', img: null })
+  const [loginError, setLoginError] = useState('')
+  const [formIsVisible, setFormIsVisible] = useState(false)
+
+  const Login = (details) => {
+    if (
+      details.email.toLowerCase() !== adminUser.email.toLowerCase() ||
+      details.password !== adminUser.password
+    ) {
+      setLoginError('Details do not match!')
+      return 1
+    }
+
+    setUser({ name: details.name, email: adminUser.email, img: adminUser.img })
+    setLoginError('')
+    return 0
+  }
+
+  // const Logout = () => {
+  //   setUser({ name: '', email: '' })
+  //   setLoginError('')
+  // }
+
+  return (
+    <div className={styles.app}>
+      {formIsVisible && (
+        <LoginForm
+          Login={Login}
+          error={loginError}
+          setFormIsVisible={setFormIsVisible}
+        />
+      )}
+      <Header user={user} setFormIsVisible={setFormIsVisible} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import styles from './Header.module.css'
+import { IoIosSearch } from 'react-icons/io'
+import { RiUserAddLine } from 'react-icons/ri'
+import { FiMail, FiBell, FiStar } from 'react-icons/fi'
+
+function Header({ user, setFormIsVisible }) {
+  const [selectIsActive, setSelectIsActive] = useState(false)
+  const [search, setSearch] = useState('')
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.navigationBlock}>
+          <a className={styles.logo} href="/">
+            <div className={styles.logo}></div>
+          </a>
+
+          <div
+            className={`${styles.select} ${
+              selectIsActive ? styles.active : ''
+            }`}
+          >
+            <select
+              onClick={() => setSelectIsActive(!selectIsActive)}
+              defaultValue="none"
+            >
+              <option value="none" disabled hidden>
+                Создать заказ
+              </option>
+              <option value="Ремонт элетроники">Ремонт элетроники</option>
+              <option value="Ремонт техники">Ремонт техники</option>
+              <option value="Уборка">Уборка</option>
+            </select>
+            <span className={styles.selectCustomArrow}></span>
+          </div>
+
+          <div className={styles.search}>
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              type="text"
+              placeholder="Что нужно сделать?"
+            />
+            <IoIosSearch className={styles.searchIcon} />
+          </div>
+        </div>
+
+        <div className={styles.userBlock}>
+          {user.name && (
+            <div className={styles.notification}>
+              <FiMail onClick={() => 'TODO: Show mails'} />
+              <FiBell onClick={() => 'TODO: Show notifiacions'} />
+              <FiStar onClick={() => 'TODO: Show favorite'} />
+            </div>
+          )}
+
+          {user.name ? (
+            <div className={styles.user} onClick={() => setFormIsVisible(true)}>
+              <div className={styles.info}>
+                <h4>{user.name}</h4>
+                <p>{user.email}</p>
+              </div>
+              <img src={user.img} alt="avatar" />
+            </div>
+          ) : (
+            <RiUserAddLine
+              className={styles.userIcon}
+              onClick={() => setFormIsVisible(true)}
+            />
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
